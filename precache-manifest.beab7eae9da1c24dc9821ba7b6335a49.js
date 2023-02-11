f20837bbd680c4202803b38dda1ea9173ee5c723
unknown <itsoxama@gmail.com>
2023-02-12 00:31:43 +0500

    first commit

---
+++ precache-manifest.beab7eae9da1c24dc9821ba7b6335a49.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd704266726b32fb29be",
+    "url": "/static/css/main.fede5ce9.chunk.css"
+  },
+  {
+    "revision": "cd704266726b32fb29be",
+    "url": "/static/js/main.42141f17.chunk.js"
+  },
+  {
+    "revision": "7e5600ec6abc175d8d8c",
+    "url": "/static/js/runtime~main.4923abb2.js"
+  },
+  {
+    "revision": "40774d360064b2c16fc8",
+    "url": "/static/css/2.68f67b3e.chunk.css"
+  },
+  {
+    "revision": "40774d360064b2c16fc8",
+    "url": "/static/js/2.9c5d8125.chunk.js"
+  },
+  {
+    "revision": "0c83c82dc2538c5868d0",
+    "url": "/static/js/3.1dcc4838.chunk.js"
+  },
+  {
+    "revision": "ac284f511f74e2c51009",
+    "url": "/static/js/4.d4a87394.chunk.js"
+  },
+  {
+    "revision": "62dfcba4d1402fa555a6",
+    "url": "/static/js/5.d0d252c5.chunk.js"
+  },
+  {
+    "revision": "960d9c1c928cebe61055dc3b0bcab8a3",
+    "url": "/static/media/219983.960d9c1c.png"
+  },
+  {
+    "revision": "1bfd7d4d8d46e92bef58aede089984de",
+    "url": "/static/media/a3.1bfd7d4d.png"
+  },
+  {
+    "revision": "de2211aecf153e0c904852cd0df2fcdd",
+    "url": "/static/media/a7.de2211ae.png"
+  },
+  {
+    "revision": "c1580a36c0cfcb72f006adbb726db827",
+    "url": "/static/media/a4.c1580a36.png"
+  },
+  {
+    "revision": "f6afad43f8903fb61bc40b8c48f0f0d8",
+    "url": "/static/media/a5.f6afad43.png"
+  },
+  {
+    "revision": "adeb0c07b94bd46478433c5bd425f0fb",
+    "url": "/static/media/a2.adeb0c07.png"
+  },
+  {
+    "revision": "7f66ce9c93f7cdd5b7f4e06bf50ae5cc",
+    "url": "/static/media/a10.7f66ce9c.png"
+  },
+  {
+    "revision": "b4002e70b6cb73b1093d83e2b8e6c733",
+    "url": "/static/media/ch.b4002e70.png"
+  },
+  {
+    "revision": "6a2238194bf361fa6d558039c94e1046",
+    "url": "/static/media/e.6a223819.png"
+  },
+  {
+    "revision": "ec1b853bfd28bf2cd98c0bbb3ca618b2",
+    "url": "/static/media/ppt.ec1b853b.png"
+  },
+  {
+    "revision": "b4002e70b6cb73b1093d83e2b8e6c733",
+    "url": "/static/media/chrome.b4002e70.png"
+  },
+  {
+    "revision": "209b6d21b355bcefbb80999a308130eb",
+    "url": "/static/media/a.209b6d21.png"
+  },
+  {
+    "revision": "962d9b3578eb83003267db05fcc5d335",
+    "url": "/static/media/a6.962d9b35.png"
+  },
+  {
+    "revision": "ad8cc12f06ed2f138a0bebc0f1b51ba1",
+    "url": "/static/media/a8.ad8cc12f.png"
+  },
+  {
+    "revision": "445fed761594a730c3ffe3d5608dbe2e",
+    "url": "/static/media/a9.445fed76.png"
+  },
+  {
+    "revision": "77d199c5b5fc1a2f7fa8683b5cfd67b6",
+    "url": "/static/media/f.77d199c5.png"
+  },
+  {
+    "revision": "4072c2b8fc8ad6147447d5e99dd2226d",
+    "url": "/static/media/y.4072c2b8.png"
+  },
+  {
+    "revision": "e9cf28830e91901357fcc818ef2b8800",
+    "url": "/static/media/w.e9cf2883.png"
+  },
+  {
+    "revision": "d0f8419b0c611225f7051070ae59cf17",
+    "url": "/static/media/vs.d0f8419b.png"
+  },
+  {
+    "revision": "308d4ff0d579222fda8c0796b48cc7fe",
+    "url": "/static/media/ss.308d4ff0.png"
+  },
+  {
+    "revision": "e3e2648ce693a0c8f5526b39aac5f50e",
+    "url": "/static/media/FontsFree-Net-aa2-13.e3e2648c.ttf"
+  },
+  {
+    "revision": "4953bff8e48fd2571eb673b2de572f61",
+    "url": "/index.html"
+  }
+];
